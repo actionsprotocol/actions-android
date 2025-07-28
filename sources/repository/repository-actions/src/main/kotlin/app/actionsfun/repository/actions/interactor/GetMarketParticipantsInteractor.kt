6699b9a7a027ee930a30728001df5b7f533b1ca4
zeroxpunk <punk.dvpn@proton.me>
2025-07-28 10:47:36 +0300

    Added get market participants

---
+++ sources/repository/repository-actions/src/main/kotlin/app/actionsfun/repository/actions/interactor/GetMarketParticipantsInteractor.kt
@@ -0,0 +1,29 @@
+package app.actionsfun.repository.actions.interactor
+
+import app.actionsfun.repository.actions.interactor.model.Market
+import app.actionsfun.repository.actions.interactor.model.Participant
+import app.actionsfun.repository.actions.internal.api.ActionsApi
+import app.actionsfun.repository.actions.internal.api.model.SerializedParticipant
+import app.actionsfun.repository.actions.internal.util.toParticipant
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+interface GetMarketParticipantsInteractor {
+
+    suspend fun get(marketAddress: String): List<Participant>
+}
+
+internal class GetMarketParticipantsInteractorImpl(
+    private val api: ActionsApi,
+) : GetMarketParticipantsInteractor {
+
+    override suspend fun get(marketAddress: String): List<Participant> {
+        return withContext(Dispatchers.IO) {
+            runCatching { api.getParticipants(marketId = marketAddress) }
+                .mapCatching { participants ->
+                    participants.map(SerializedParticipant::toParticipant)
+                }
+                .getOrThrow()
+        }
+    }
+}
