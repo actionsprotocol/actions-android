4aa7011b82ad17cbccc87b9de85b818dc2b7508f
zeroxpunk <punk.dvpn@proton.me>
2025-07-28 10:50:50 +0300

    Added get participated markets

---
+++ sources/repository/repository-actions/src/main/kotlin/app/actionsfun/repository/actions/interactor/GetParticipatedMarkets.kt
@@ -0,0 +1,51 @@
+package app.actionsfun.repository.actions.interactor
+
+import app.actionsfun.repository.actions.interactor.model.Market
+import app.actionsfun.repository.actions.internal.api.ActionsApi
+import app.actionsfun.repository.actions.internal.util.lamportsToSOL
+import app.actionsfun.repository.actions.internal.util.parseTimestamp
+import app.actionsfun.repository.solana.WalletRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+interface GetParticipatedMarkets {
+
+    suspend fun get(): List<Market>
+}
+
+internal class GetParticipatedMarketsImpl(
+    private val api: ActionsApi,
+    private val walletRepository: WalletRepository,
+) : GetParticipatedMarkets {
+
+    override suspend fun get(): List<Market> {
+        return withContext(Dispatchers.IO) {
+            val wallet = walletRepository.getWallet()
+
+            check(!wallet.isNone) {
+                "Wallet is not connected"
+            }
+
+            val marketData = api.getParticipatedMarkets(wallet.publicKey)
+                .data.orEmpty()
+
+            marketData.map { market ->
+                Market(
+                    address = market.address,
+                    title = market.name,
+                    description = market.description,
+                    image = market.metadataUri,
+                    creatorTwitterUsername = market.creatorTwitterUsername.orEmpty(),
+                    creatorTwitterImage = market.creatorTwitterImage.orEmpty(),
+                    createdAt = market.createdTimestamp.parseTimestamp(),
+                    endsAt = market.expiryTimestamp.parseTimestamp(),
+                    uiState = market.uiState,
+                    volume = market.totalMarketSize.lamportsToSOL(),
+                    yesVolume = market.yesAmount.lamportsToSOL(),
+                    noVolume = market.noAmount.lamportsToSOL(),
+                    replies = emptyList(),
+                )
+            }
+        }
+    }
+}
