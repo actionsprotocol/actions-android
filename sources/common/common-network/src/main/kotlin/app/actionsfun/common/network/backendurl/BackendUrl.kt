42f8dc4fc58974a2f706b47f4538402714901788
zeroxpunk <punk.dvpn@proton.me>
2025-08-02 18:43:17 +0300

    Updated backend url

--- sources/common/common-network/src/main/kotlin/app/actionsfun/common/network/backendurl/BackendUrl.kt
+++ sources/common/common-network/src/main/kotlin/app/actionsfun/common/network/backendurl/BackendUrl.kt
@@ -5,13 +5,19 @@ import app.actionsfun.common.network.BuildConfig.BUILD_TYPE
 sealed interface BackendUrl {
 
     val app: String
+    val pinataGateway: String
+    val pinataBaseUrl: String
 
     data object Dev : BackendUrl {
         override val app = "https://actions-backend-azil.onrender.com/"
+        override val pinataGateway = "https://gateway.pinata.cloud"
+        override val pinataBaseUrl = "https://sapphire-top-harrier-590.mypinata.cloud"
     }
 
     data object Prod : BackendUrl {
         override val app = "https://actions-backend-mainnet.onrender.com/"
+        override val pinataGateway = "https://gateway.pinata.cloud"
+        override val pinataBaseUrl = "https://sapphire-top-harrier-590.mypinata.cloud"
     }
 
     @Suppress("KotlinConstantConditions")
