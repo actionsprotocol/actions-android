b85d06f1172e30fb806ed43fe99e1166220a7621
zeroxpunk <punk.dvpn@proton.me>
2025-08-02 18:36:48 +0300

    Added pinata support

--- sources/repository/repository-actions/build.gradle.kts
+++ sources/repository/repository-actions/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation(projects.sources.repository.repositoryPreferences)
     implementation(projects.sources.repository.repositoryUser)
     implementation(projects.sources.repository.repositorySolana)
+    implementation(projects.sources.repository.repositoryPinata)
     implementation(projects.sources.common.commonNetwork)
     implementation(projects.sources.common.commonUtil)
     implementation(libs.timber)
